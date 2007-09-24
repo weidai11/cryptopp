5ec62cd9b743ba20510f23dcf66a1db72d1979f0
weidai <weidai@57ff6487-cd31-0410-9ec3-f628ee90f5f0>
2007-09-24 03:52:17 +0000

    remove -msse2 since we don't use SSE2 intrinsics anymore

--- trunk/c5/salsa.cpp
+++ trunk/c5/salsa.cpp
@@ -467,7 +467,7 @@ void Salsa20_Policy::OperateKeystream(KeystreamOperation operation, byte *output
 			: "%eax", "%edx", "memory", "cc", "%xmm0", "%xmm1", "%xmm2", "%xmm3", "%xmm4", "%xmm5", "%xmm6", "%xmm7", "%xmm8", "%xmm9", "%xmm10", "%xmm11", "%xmm12", "%xmm13", "%xmm14", "%xmm15"
 	#else
 			: "m" (r), "a" (input), "c" (iterationCount), "S" (m_state.data()), "D" (output)
-			: "%edx", "memory", "cc", "%xmm0", "%xmm1", "%xmm2", "%xmm3", "%xmm4", "%xmm5", "%xmm6", "%xmm7"
+			: "%edx", "memory", "cc"
 	#endif
 		);
 #endif
