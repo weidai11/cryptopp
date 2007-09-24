da23735ea2cc501206f850625f83e3d9167f7f11
weidai <weidai@57ff6487-cd31-0410-9ec3-f628ee90f5f0>
2007-09-24 00:45:59 +0000

    - port x64 assembly code to MASM - improve stack unwindability on x64 for GCC by not modifying RBP/RSP registers in inline assembly

--- trunk/c5/pch.h
+++ trunk/c5/pch.h
@@ -1,13 +1,21 @@
 #ifndef CRYPTOPP_PCH_H
 #define CRYPTOPP_PCH_H
 
-#include "config.h"
+#ifdef CRYPTOPP_GENERATE_X64_MASM
 
-#ifdef USE_PRECOMPILED_HEADERS
-#include "simple.h"
-#include "secblock.h"
-#include "misc.h"
-#include "smartptr.h"
+	#include "cpu.h"
+
+#else
+
+	#include "config.h"
+
+	#ifdef USE_PRECOMPILED_HEADERS
+		#include "simple.h"
+		#include "secblock.h"
+		#include "misc.h"
+		#include "smartptr.h"
+	#endif
+
 #endif
 
 #endif
