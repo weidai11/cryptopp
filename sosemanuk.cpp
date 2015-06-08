24a68684e8ad47f1a2093bbb626ac654d92083b1
Jeffrey Walton <noloader@gmail.com>
2015-06-08 02:26:00 -0400

    Cleared unknown pragma warning with -Wall

--- sosemanuk.cpp
+++ sosemanuk.cpp
@@ -317,7 +317,9 @@ void Sosemanuk_OperateKeystream(size_t iterationCount, const byte *input, byte *
 }
 #endif
 
-#pragma warning(disable: 4731)	// frame pointer register 'ebp' modified by inline assembly code
+#ifdef _MSC_VER
+# pragma warning(disable: 4731)	// frame pointer register 'ebp' modified by inline assembly code
+#endif
 
 void SosemanukPolicy::OperateKeystream(KeystreamOperation operation, byte *output, const byte *input, size_t iterationCount)
 {
