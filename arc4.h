f13511947d516ac6b80b4928d06b134087518c62
Jeffrey Walton <noloader@gmail.com>
2015-07-20 12:19:18 -0400

    Whitespace checkin

--- arc4.h
+++ arc4.h
@@ -18,7 +18,7 @@ public:
 	void GenerateBlock(byte *output, size_t size);
 	void DiscardBytes(size_t n);
 
-    void ProcessData(byte *outString, const byte *inString, size_t length);
+	void ProcessData(byte *outString, const byte *inString, size_t length);
 	
 	bool IsRandomAccess() const {return false;}
 	bool IsSelfInverting() const {return true;}
