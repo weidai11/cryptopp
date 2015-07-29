d41009f1dc56e324000070b17f4444f9058abe07
Jeffrey Walton <noloader@gmail.com>
2015-07-29 13:45:54 -0400

    Added script to clean Visual Studio artifacts

---
+++ vs-clean.cmd
@@ -0,0 +1,26 @@
+@echo OFFse
+REM set THIS_DIR=%~dp0
+set THIS_DIR=.
+
+attrib -R -A -S -H  "%THIS_DIR%\*.suo"
+attrib -R -A -S -H  "%THIS_DIR%\*.sdf"
+attrib -R -A -S -H  "%THIS_DIR%\*.vcxproj"
+attrib -R -A -S -H  "%THIS_DIR%\*.filters"
+
+del "%THIS_DIR%\*.suo" /q
+del "%THIS_DIR%\*.sdf" /q
+del "%THIS_DIR%\*.filters" /q
+del "%THIS_DIR%\*.vcxproj" /q
+del "%THIS_DIR%\*.diff" /q
+del "%THIS_DIR%\adhoc.cpp" /q
+del "%THIS_DIR%\adhoc.cpp.copied" /q
+del  "%THIS_DIR%\UpgradeLog.htm" /q
+
+REM Visual Studio project conversion
+rmdir /Q /S "%THIS_DIR%\Backup\"
+rmdir /Q /S "%THIS_DIR%\Debug\"
+rmdir /Q /S "%THIS_DIR%\Release\"
+rmdir /Q /S "%THIS_DIR%\Win32\"
+rmdir /Q /S "%THIS_DIR%\x64\"
+rmdir /Q /S "%THIS_DIR%\ipch\"
+rmdir /Q /S "%THIS_DIR%\.vs\"
