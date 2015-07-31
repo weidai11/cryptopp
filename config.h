9fd44748c15491e332c5fcb8b37c37594ce1ec5d
Jeffrey Walton <noloader@gmail.com>
2015-07-31 02:11:32 -0400

    Added define for CRYPTOPP_CXX14 so we can pick up operator "" and small string optimizations (SSO)

--- config.h
+++ config.h
@@ -505,13 +505,19 @@ NAMESPACE_END
 // ***************** C++11 related ********************
 
 // Visual Studio and C++11 language features began at Visual Studio 2010,
-// https://msdn.microsoft.com/en-us/library/hh567368%28v=vs.110%29.aspx.
+//   https://msdn.microsoft.com/en-us/library/hh567368%28v=vs.110%29.aspx.
 // Also see the warning on the buggy C++11 feature matrix at
-// http://stackoverflow.com/a/31642496/608639
+//   http://stackoverflow.com/a/31642496/608639
 #if (_MSC_VER >= 1600) || (__cplusplus >= 201103L)
 # define CRYPTOPP_CXX11 1
 #endif
-
+	
+// C++14 adds a operator”” and Small String Optimizations (SSO)
+// TODO: change this when Microsoft adds support
+#if (_MSC_VER >= 1600) || (__cplusplus >= 201402L)
+# define CRYPTOPP_CXX14 1
+#endif 
+	
 // C++11 is available
 #if defined(CRYPTOPP_CXX11)
 
