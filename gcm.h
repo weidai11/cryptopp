b6a32c063af00bda0acec41dfceed84c28f9a15e
Jeffrey Walton <noloader@gmail.com>
2016-01-29 09:59:09 -0500

    Updated documentation

--- gcm.h
+++ gcm.h
@@ -1,7 +1,6 @@
 // gcm.h - written and placed in the public domain by Wei Dai
 
-//! \file
-//! \headerfile gcm.h
+//! \file gcm.h
 //! \brief GCM block cipher mode of operation
 
 #ifndef CRYPTOPP_GCM_H
@@ -13,12 +12,16 @@
 NAMESPACE_BEGIN(CryptoPP)
 
 //! \enum GCM_TablesOption
-//! \brief Use either 2K or 64K size tables.
-enum GCM_TablesOption {GCM_2K_Tables, GCM_64K_Tables};
+//! \brief GCM table size options
+enum GCM_TablesOption {
+	//! \brief Use a table with 2K entries
+	GCM_2K_Tables,
+	//! \brief Use a table with 64K entries
+	GCM_64K_Tables};
 
 //! \class GCM_Base
-//! \brief CCM block cipher mode of operation.
-//! \details Implementations and overrides in \p GCM_Base apply to both \p ENCRYPTION and \p DECRYPTION directions
+//! \brief GCM block cipher base implementation
+//! \details Base implementation of the AuthenticatedSymmetricCipher interface
 class CRYPTOPP_DLL CRYPTOPP_NO_VTABLE GCM_Base : public AuthenticatedSymmetricCipherBase
 {
 public:
@@ -87,13 +90,10 @@ protected:
 };
 
 //! \class GCM_Final
-//! \brief Class specific methods used to operate the cipher.
+//! \brief GCM block cipher final implementation
 //! \tparam T_BlockCipher block cipher
 //! \tparam T_TablesOption table size, either \p GCM_2K_Tables or \p GCM_64K_Tables
 //! \tparam T_IsEncryption direction in which to operate the cipher
-//! \details Implementations and overrides in \p GCM_Final apply to either
-//!   \p ENCRYPTION or \p DECRYPTION, depending on the template parameter \p T_IsEncryption.
-//! \details \p GCM_Final does not use inner classes \p Enc and \p Dec.
 template <class T_BlockCipher, GCM_TablesOption T_TablesOption, bool T_IsEncryption>
 class GCM_Final : public GCM_Base
 {
@@ -110,7 +110,7 @@ private:
 };
 
 //! \class GCM
-//! \brief The GCM mode of operation
+//! \brief GCM block cipher mode of operation
 //! \tparam T_BlockCipher block cipher
 //! \tparam T_TablesOption table size, either \p GCM_2K_Tables or \p GCM_64K_Tables
 //! \details \p GCM provides the \p Encryption and \p Decryption typedef.
