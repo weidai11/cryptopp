df1c94a38a97119198aa3ae92e82ab9e46d4e9b5
Jeffrey Walton <noloader@gmail.com>
2016-04-23 18:47:23 -0400

    Initial cut-in of CRYPTOPP_USE_FIPS_202_SHA3 macro

--- sha3.cpp
+++ sha3.cpp
@@ -280,7 +280,13 @@ void SHA3::Restart()
 void SHA3::TruncatedFinal(byte *hash, size_t size)
 {
 	ThrowIfInvalidTruncatedSize(size);
+
+#if defined(CRYPTOPP_USE_FIPS_202_SHA3)
 	m_state.BytePtr()[m_counter] ^= 0x06;
+#else
+	m_state.BytePtr()[m_counter] ^= 1;
+#endif
+
 	m_state.BytePtr()[r()-1] ^= 0x80;
 	KeccakF1600(m_state);
 	memcpy(hash, m_state, size);
