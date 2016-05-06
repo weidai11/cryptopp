9711fd1d4415866141b834becf447ba0a6a75361
Jeffrey Walton <noloader@gmail.com>
2016-05-06 01:28:06 -0400

    Add Windows ARM NEON dtection

--- cpu.cpp
+++ cpu.cpp
@@ -23,9 +23,11 @@
 #endif
 
 #if CRYPTOPP_BOOL_NEON_INTRINSICS_AVAILABLE
-#include <sys/auxv.h>
-#include <asm/hwcap.h>
-#include <arm_neon.h>
+# if defined(__linux__)
+#  include <sys/auxv.h>
+#  include <asm/hwcap.h>
+# endif
+# include <arm_neon.h>
 #endif
 
 NAMESPACE_BEGIN(CryptoPP)
