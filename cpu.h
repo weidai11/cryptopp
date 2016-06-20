58ef92d0e3337bbba30df962349116e56217e813
Jeffrey Walton <noloader@gmail.com>
2016-06-20 11:31:15 -0400

    Fix "SSE instruction set not enabled" (Issue 196)

--- cpu.h
+++ cpu.h
@@ -41,6 +41,8 @@
 #  include <emmintrin.h>
 # endif
 
+#if CRYPTOPP_BOOL_AESNI_INTRINSICS_AVAILABLE
+
 // GCC 5.3/i686 fails to declare __m128 in the headers we use when compiling with -std=c++11 or -std=c++14.
 // Consequently, our _mm_shuffle_epi8, _mm_extract_epi32, etc fails to compile.
 #if defined(__has_include)
@@ -50,7 +52,6 @@
 #endif
 
 // PUSHFB needs Clang 3.3 and Apple Clang 5.0.
-#if CRYPTOPP_BOOL_AESNI_INTRINSICS_AVAILABLE
 #if !defined(__GNUC__) || defined(__SSSE3__)|| defined(__INTEL_COMPILER) || (CRYPTOPP_CLANG_VERSION >= 30300) || (CRYPTOPP_APPLE_CLANG_VERSION >= 50000)
 #include <tmmintrin.h>
 #else
