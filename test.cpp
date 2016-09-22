c7303a600f53aedab8f0ec964f4bced6aa663cf6
Jeffrey Walton <noloader@gmail.com>
2016-09-22 00:11:35 -0400

    Cleared Coverity finding CID 12086 (Issue 299)

--- test.cpp
+++ test.cpp
@@ -359,9 +359,9 @@ int CRYPTOPP_API main(int argc, char *argv[])
 		else if (command == "v" || command == "vv")
 			return !Validate(argc>2 ? StringToValue<int, true>(argv[2]) : 0, argv[1][1] == 'v', argc>3 ? argv[3] : NULL);
 		else if (command == "b")
-			BenchmarkAll(argc<3 ? 1 : StringToValue<float, true>(argv[2]), argc<4 ? 0 : StringToValue<float, true>(argv[3])*1e9);
+			BenchmarkAll(argc<3 ? 1 : StringToValue<float, true>(argv[2]), argc<4 ? 0.0f : StringToValue<float, true>(argv[3])*1e9);
 		else if (command == "b2")
-			BenchmarkAll2(argc<3 ? 1 : StringToValue<float, true>(argv[2]), argc<4 ? 0 : StringToValue<float, true>(argv[3])*1e9);
+			BenchmarkAll2(argc<3 ? 1 : StringToValue<float, true>(argv[2]), argc<4 ? 0.0f : StringToValue<float, true>(argv[3])*1e9);
 		else if (command == "z")
 			GzipFile(argv[3], argv[4], argv[2][0]-'0');
 		else if (command == "u")
@@ -432,10 +432,10 @@ template <class T, bool NON_NEGATIVE>
 T StringToValue(const std::string& str) {
 	std::istringstream iss(str);
 	T value;
-	iss >> value;
+	iss >> std::noskipws >> value;
 
 	// Use fail(), not bad()
-	if (iss.fail())
+	if (iss.fail() || !iss.eof())
 		throw InvalidArgument("cryptest.exe: '" + str +"' is not a value");
 
 	if (NON_NEGATIVE && value < 0)
