f61b9eda2e9704dd6a49cdfb26c8d38e5469d300
Jeffrey Walton <noloader@gmail.com>
2016-09-29 13:09:10 -0400

    Add Coverity modeling file

---
+++ TestScripts/cryptest-coverity.cpp
@@ -0,0 +1,13 @@
+// cryptest-coverity.cpp - Coverity modeling file.
+//                         Written and placed in public domain by Jeffrey Walton and Uri Blumenthal.
+//                         Copyright assigned to Crypto++ project.
+//
+// For more information see http://cryptopp.com/wiki/Coverity_Scan.
+//
+// Also see https://scan.coverity.com/tune#what-is-model
+
+///////////////////////////////////////////////////////////////////
+
+void special_abort(const char* msg) {
+	__coverity_panic__();
+}
