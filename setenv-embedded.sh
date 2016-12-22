182f87eeacea3d6571100bf7e486177798a5c1d9
Jeffrey Walton <noloader@gmail.com>
2016-12-22 05:24:16 -0500

    Add info on creating versioned shared object Also see http://stackoverflow.com/q/41255199

--- setenv-embedded.sh
+++ setenv-embedded.sh
@@ -134,8 +134,9 @@ fi
 
 echo
 echo "*******************************************************************************"
-echo "It looks the the environment is set correctly. Your next step is"
-echo "build the library with 'make -f GNUmakefile-cross'"
+echo "It looks the the environment is set correctly. Your next step is build"
+echo "the library with 'make -f GNUmakefile-cross'. You can create a versioned"
+echo "shared object using 'HAS_SOLIB_VERSION=1 make -f GNUmakefile-cross'"
 echo "*******************************************************************************"
 echo
 
