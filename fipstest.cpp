3af4765e153ce3f38cc99d11722d2c6c4a09e928
RaptorFactor <raptorfactor@raptorfactor.com>
2017-03-25 23:14:12 -0700

    Fix linking of fipstest for MSVC targeting ARM (__crt_debugger_hook is not available).

--- fipstest.cpp
+++ fipstest.cpp
@@ -400,7 +400,7 @@ bool IntegrityCheckModule(const char *moduleFilename, const byte *expectedModule
 					}
 				}
 
-#if defined(_MSC_VER) && _MSC_VER >= 1400
+#if defined(_MSC_VER) && _MSC_VER >= 1400 && !defined(_M_ARM)
 				// first byte of _CRT_DEBUGGER_HOOK gets modified in memory by the debugger invisibly, so read it from file
 				if (IsDebuggerPresent())
 				{
