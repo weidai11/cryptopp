a8b33272654389b0ce9d5935ae4acc861794e7c8
Jeffrey Walton <noloader@gmail.com>
2017-08-07 11:33:12 -0400

    Add Android CPU feature detection for NEON Also see https://developer.android.com/ndk/guides/cpu-features.html

--- neon.cpp
+++ neon.cpp
@@ -73,8 +73,17 @@ bool CPU_TryNEON_ARM()
 	}
 	return result;
 # else
-#   if defined(__linux__) && (defined(__aarch32__) || defined(__aarch64__))
+#   if defined(__android__) && defined(CRYPTOPP_BOOL_ARM64)
+	if (android_getCpuFeatures() & ANDROID_CPU_ARM64_FEATURE_ASIMD)
+		return true;
+#   elif defined(__android__) && defined(CRYPTOPP_BOOL_ARM32)
+	if (android_getCpuFeatures() & ANDROID_CPU_ARM_FEATURE_NEON)
+		return true;
+#   elif defined(__linux__) && defined(__aarch64__)
 	if (getauxval(AT_HWCAP) & HWCAP_ASIMD)
+		return true;
+#   elif defined(__linux__) && defined(__aarch32__)
+	if (getauxval(AT_HWCAP2) & HWCAP2_ASIMD)
 		return true;
 #   elif defined(__linux__) && defined(__arm__)
 	if (getauxval(AT_HWCAP) & HWCAP_ARM_NEON)
