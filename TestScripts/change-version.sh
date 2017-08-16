e19fad007fde3bc744c9c7b1ebdbd7d49ae6a185
Jeffrey Walton <noloader@gmail.com>
2017-08-16 05:36:11 -0400

    Fix library version number in script

--- TestScripts/change-version.sh
+++ TestScripts/change-version.sh
@@ -4,6 +4,6 @@
 # building the docs. Before running the script, copy it to the root
 # directory. After running this script, you can 'make docs'
 
-gsed -i 's|Library 6.0 API|Library 5.6.5 API|g' cryptlib.h
-gsed -i 's|= 6.0.0|= 5.6.5|g' Doxyfile
-gsed -i 's|CRYPTOPP_VERSION 600|CRYPTOPP_VERSION 565|g' config.h
+sed -i 's|Library 6.0 API|Library 5.6.5 API|g' cryptlib.h
+sed -i 's|= 6.0|= 5.6.5|g' Doxyfile
+sed -i 's|CRYPTOPP_VERSION 600|CRYPTOPP_VERSION 565|g' config.h
