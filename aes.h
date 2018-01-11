591d70f1c7a92072765abe8dc0d3fbaf18411505
Jeffrey Walton <noloader@gmail.com>
2018-01-11 14:20:59 -0500

    Update documentation

--- aes.h
+++ aes.h
@@ -4,7 +4,8 @@
 /// \brief Class file for the AES cipher (Rijndael)
 /// \details AES is a typdef for Rijndael classes. All key sizes are supported.
 ///   The library only provides Rijndael with 128-bit blocks, and not 192-bit or 256-bit blocks
-/// \since Rijndael since Crypto++ 3.2, Intel AESNI since Crypto++ 5.6.1, ARMv8 AES since Crypto++ 6.0
+/// \since Rijndael since Crypto++ 3.2, Intel AES-NI since Crypto++ 5.6.1, ARMv8 AES since
+///   Crypto++ 6.0, Power8 AES since Crypto++ 6.0
 
 #ifndef CRYPTOPP_AES_H
 #define CRYPTOPP_AES_H
