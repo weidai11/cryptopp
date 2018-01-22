91a8652f9e768eb77c3901cd5db099e7ea7ef725
Jeffrey Walton <noloader@gmail.com>
2018-01-22 18:22:34 -0500

    Update version info

--- TestScripts/change-version.sh
+++ TestScripts/change-version.sh
@@ -4,6 +4,6 @@
 # building the docs. Before running the script, copy it to the root
 # directory. After running this script, you can 'make docs'
 
-sed -i 's|Library 6.0 API|Library 5.6.5 API|g' cryptlib.h
-sed -i 's|= 6.0|= 5.6.5|g' Doxyfile
-sed -i 's|CRYPTOPP_VERSION 600|CRYPTOPP_VERSION 565|g' config.h
+sed -i 's|Library 6.1 API|Library 6.0 API|g' cryptlib.h
+sed -i 's|= 6.1|= 6.0|g' Doxyfile
+sed -i 's|CRYPTOPP_VERSION 610|CRYPTOPP_VERSION 600|g' config.h
