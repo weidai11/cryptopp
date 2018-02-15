6e8f6630cf507ab60fbd6fddd3092912e5610e54
Jan Beich <jbeich@FreeBSD.org>
2018-02-15 02:35:46 +0000

    Fix Clang check for C++11 lambdas (GH #558, citra-emu/citra#3435) (#587)
    $ CXXFLAGS=-std=gnu++17 gmake
    clang++ -std=gnu++17 -fPIC -pthread -pipe -c cryptlib.cpp
    In file included from cryptlib.cpp:19:
    ./misc.h:2542:43: error: no member named 'bind2nd' in namespace 'std'
        return std::find_if(first, last, std::bind2nd(std::not_equal_to<T>(), value));
                                         ~~~~~^
    1 error generated.

--- config.h
+++ config.h
@@ -1026,7 +1026,7 @@ NAMESPACE_END
 #endif // alignof
 
 // lambdas: MS at VS2012 (17.00); GCC at 4.9; Clang at 3.3; Intel 12.0; SunCC 5.14.
-#if (CRYPTOPP_MSC_VERSION >= 1700) || __has_feature(cxx_lambda) || \
+#if (CRYPTOPP_MSC_VERSION >= 1700) || __has_feature(cxx_lambdas) || \
 	(__INTEL_COMPILER >= 1200) || (CRYPTOPP_GCC_VERSION >= 40900) || (__SUNPRO_CC >= 0x5140)
 #  define CRYPTOPP_CXX11_LAMBDA 1
 #endif // lambdas
