bdd0f02867ff843be25c284258d3256a48e41cb1
Jeffrey Walton <noloader@gmail.com>
2018-04-08 16:19:16 -0400

    Make chenage-version.sh OS X friendly

--- TestScripts/change-version.sh
+++ TestScripts/change-version.sh
@@ -4,6 +4,11 @@
 # building the docs. Before running the script, copy it to the root
 # directory. After running this script, you can 'make docs'
 
-sed -i 's|Library 7.1 API|Library 7.0 API|g' cryptlib.h
-sed -i 's|= 7.1|= 7.0|g' Doxyfile
-sed -i 's|CRYPTOPP_VERSION 710|CRYPTOPP_VERSION 700|g' config.h
+sed 's|Library 7.1 API|Library 7.0 API|g' cryptlib.h > cryptlib.h.new
+mv cryptlib.h.new cryptlib.h
+
+sed 's|= 7.1|= 7.0|g' Doxyfile > Doxyfile.new
+mv Doxyfile.new Doxyfile
+
+sed 's|CRYPTOPP_VERSION 710|CRYPTOPP_VERSION 700|g' config.h > config.h.new
+mv config.h.new config.h
