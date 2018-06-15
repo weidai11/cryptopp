20256ad7b60de93a94674da22922d827251fd8f1
Jeffrey Walton <noloader@gmail.com>
2018-06-15 01:31:16 -0400

    Use default alignment for GetBlock Change background color for lightweight block ciphers

--- bench1.cpp
+++ bench1.cpp
@@ -592,7 +592,7 @@ void Benchmark2(double t, double hertz)
 		BenchMarkByName<SymmetricCipher>("Threefish-1024(1024)/CTR", 128);
 		BenchMarkByName<SymmetricCipher>("Serpent/CTR");
 		BenchMarkByName<SymmetricCipher>("CAST-128/CTR");
-		BenchMarkByName<SymmetricCipher>("CAST-256/CTR");
+		BenchMarkByName<SymmetricCipher>("CAST-256/CTR", 32);
 		BenchMarkByName<SymmetricCipher>("RC6/CTR");
 		BenchMarkByName<SymmetricCipher>("MARS/CTR");
 		BenchMarkByName<SymmetricCipher>("SHACAL-2/CTR", 16);
@@ -614,6 +614,7 @@ void Benchmark2(double t, double hertz)
 		BenchMarkByName<SymmetricCipher>("Kalyna-256/CTR", 32, "Kalyna-256(256)/CTR (256-bit key)");
 		BenchMarkByName<SymmetricCipher>("Kalyna-256/CTR", 64, "Kalyna-256(512)/CTR (512-bit key)");
 		BenchMarkByName<SymmetricCipher>("Kalyna-512/CTR", 64, "Kalyna-512(512)/CTR (512-bit key)");
+	}
 
 		BenchMarkByName<SymmetricCipher>("LEA-128/CTR", 16, "LEA-128(128)/CTR (128-bit key)");
 		BenchMarkByName<SymmetricCipher>("LEA-128/CTR", 24, "LEA-128(192)/CTR (192-bit key)");
@@ -636,7 +637,7 @@ void Benchmark2(double t, double hertz)
 		BenchMarkByName<SymmetricCipher>("CHAM-128/CTR", 32, "CHAM-128(256)/CTR (256-bit key)");
 	}
 
-	std::cout << "\n<TBODY style=\"background: yellow;\">";
+	std::cout << "\n<TBODY style=\"background: white;\">";
 	{
 #if CRYPTOPP_AESNI_AVAILABLE
 		if (HasCLMUL())
