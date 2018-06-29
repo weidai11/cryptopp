9a75d09ca48becc2e0dc675b34b0c16bfb5da7c1
Jeffrey Walton <noloader@gmail.com>
2018-06-29 19:43:31 -0400

    Add cleanup function to cryptest-cmake.sh

--- TestScripts/cryptest-cmake.sh
+++ TestScripts/cryptest-cmake.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+PWD_DIR=$(pwd)
+function cleanup {
+    cd "$PWD_DIR"
+}
+trap cleanup EXIT
+
 # Feth the three required files
 if ! wget --no-check-certificate https://raw.githubusercontent.com/noloader/cryptopp-cmake/master/CMakeLists.txt -O CMakeLists.txt; then
 	echo "CMakeLists.txt download failed"
@@ -22,29 +28,23 @@ cd "$PWD_DIR/build"
 
 if ! cmake ../; then
 	echo "autoreconf failed"
-	cd "$PWD_DIR"
 	[[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
 fi
 
 if ! make -j2 -f Makefile VERBOSE=1; then
 	echo "make failed"
-	cd "$PWD_DIR"
 	[[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
 fi
 
 if ! ./cryptest.exe v; then
 	echo "cryptest.exe v failed"
-	cd "$PWD_DIR"
 	[[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
 fi
 
 if ! ./cryptest.exe tv all; then
 	echo "cryptest.exe v failed"
-	cd "$PWD_DIR"
 	[[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
 fi
-
-cd "$PWD_DIR"
 
 # Return success
 [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 0 || return 0
