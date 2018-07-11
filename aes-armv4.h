d094bc61cd9053b326f58621f00ce5360f8e01da
Jeffrey Walton <noloader@gmail.com>
2018-07-11 15:12:53 -0400

    Update comments

--- aes-armv4.h
+++ aes-armv4.h
@@ -1,5 +1,5 @@
 /* Header file for use with Cryptogam's ARMv4 AES.         */
-/* Also see  http://www.openssl.org/~appro/cryptogams/ and */
+/* Also see http://www.openssl.org/~appro/cryptogams/ and  */
 /* https://wiki.openssl.org/index.php?title=Cryptogams_AES */
 
 #ifndef CRYPTOGAMS_AES_ARMV4_H
@@ -9,15 +9,14 @@
 extern "C" {
 #endif
 
-#define AES_MAXNR 14
-
+//#define AES_MAXNR 14
 //typedef struct AES_KEY_st {
 //    unsigned int rd_key[4 * (AES_MAXNR + 1)];
 //    int rounds;
 //} AES_KEY;
 
 // Instead of AES_KEY we use a 'word32 rkey[4*15+4]'. It has space for
-// both the AES_MAXNR round keys and the round numbers in the tail.
+// both the AES_MAXNR round keys and the number of rounds in the tail.
 
 int AES_set_encrypt_key(const unsigned char *userKey, const int bits, unsigned int *rkey);
 int AES_set_decrypt_key(const unsigned char *userKey, const int bits, unsigned int *rkey);
