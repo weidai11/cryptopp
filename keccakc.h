b9a6034a816b8cfce728745d16bd4880b4f1171c
Jeffrey Walton <noloader@gmail.com>
2018-08-22 00:40:30 -0400

    Add keccakc.h and keccakc.cpp for shared F1600

---
+++ keccakc.h
@@ -0,0 +1,13 @@
+// keccakc.h - Keccak core functions shared between SHA3 and Keccak.
+//             written and placed in the public domain by JW.
+
+#ifndef CRYPTOPP_KECCAK_CORE
+#define CRYPTOPP_KECCAK_CORE
+
+NAMESPACE_BEGIN(CryptoPP)
+
+void KeccakF1600(word64 *state);
+
+NAMESPACE_END
+
+#endif  // CRYPTOPP_KECCAK_CORE
