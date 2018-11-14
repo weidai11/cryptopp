b3941a433db3f6b26b66b7db67580a0ecd97ae09
Jeffrey Walton <noloader@gmail.com>
2018-11-13 23:39:39 -0500

    Fix some IBM XL C/C++ issues

--- TestPrograms/test_cxx.cxx
+++ TestPrograms/test_cxx.cxx
@@ -1,7 +1,6 @@
 #include <string>
-#include <cstdint>
 int main(int argc, char* argv[])
 {
-	uint32_t x=0;
+	unsigned int x=0;
 	return x;
 }
--- TestPrograms/test_x86_cpuid.cxx
+++ TestPrograms/test_x86_cpuid.cxx
@@ -1,7 +1,6 @@
-#include <stdint.h>
 int main(int argc, char* argv[])
 {
-	uint32_t a, b, c, d;
+	unsigned int a, b, c, d;
 	asm volatile ( "cpuid" : "+a"(a), "=b"(b), "+c"(c), "=d"(d) );
 
 	return 0;
