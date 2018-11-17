b0f77fe7e3c3acedfa5ceddbfb30cc5738bcdc34
Jeffrey Walton <noloader@gmail.com>
2018-11-17 15:09:36 -0500

    Add additional tests

---
+++ TestPrograms/test_32bit.cxx
@@ -0,0 +1,6 @@
+int main(int argc, char* argv[])
+{
+	enum {N = (sizeof(size_t) == 4 ? 4 : -1)};
+	int x[N];
+	return 0;
+}
---
+++ TestPrograms/test_64bit.cxx
@@ -0,0 +1,6 @@
+int main(int argc, char* argv[])
+{
+	enum {N = (sizeof(size_t) == 8 ? 8 : -1)};
+	int x[N];
+	return 0;
+}
