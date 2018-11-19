2bb9f49544d90740a0c5db3472a32e6581a85f92
Jeffrey Walton <noloader@gmail.com>
2018-11-18 22:26:34 -0500

    Add test_ppc_aes.cxx test program

--- TestPrograms/test_ppc_power8.cxx
+++ TestPrograms/test_ppc_power8.cxx
@@ -2,18 +2,6 @@
 int main(int argc, char* argv[])
 {
 	__vector unsigned long long z;
-#if defined(__xlc__) || defined(__xlC__) || defined(__clang__)
-	__vector unsigned char x;
-	x=__vcipher(x,x);
-	x=__vcipherlast(x,x);
-	x=__vncipher(x,x);
-	x=__vncipherlast(x,x);
-#elif defined(__GNUC__)
-	__vector unsigned long long x;
-	x=__builtin_crypto_vcipher(x,x);
-	x=__builtin_crypto_vcipherlast(x,x);
-	x=__builtin_crypto_vncipher(x,x);
-	x=__builtin_crypto_vncipherlast(x,x);
-#endif
+	z=vec_add(z,z);
 	return 0;
 }
