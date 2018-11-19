c9f1a26024d6e099f4c7e57e9ca8391a780dbe1a
Jeffrey Walton <noloader@gmail.com>
2018-11-19 02:02:43 -0500

    Update test programs for PowerPC (GH #741)

--- TestPrograms/test_ppc_aes.cxx
+++ TestPrograms/test_ppc_aes.cxx
@@ -2,19 +2,19 @@
 int main(int argc, char* argv[])
 {
 #if defined(__ibmxl__) || (defined(_AIX) && defined(__xlC__))
-	__vector unsigned char x;
+	__vector unsigned char x = {1,2,3,4,5,6,7,8};
 	x=__vcipher(x,x);
 	x=__vcipherlast(x,x);
 	x=__vncipher(x,x);
 	x=__vncipherlast(x,x);
 #elif defined(__clang__)
-	__vector unsigned long long x;
+	__vector unsigned long long x = {1,2};
 	x=__builtin_altivec_crypto_vcipher(x,x);
 	x=__builtin_altivec_crypto_vcipherlast(x,x);
 	x=__builtin_altivec_crypto_vncipher(x,x);
 	x=__builtin_altivec_crypto_vncipherlast(x,x);
 #elif defined(__GNUC__)
-	__vector unsigned long long x;
+	__vector unsigned long long x = {1,2};
 	x=__builtin_crypto_vcipher(x,x);
 	x=__builtin_crypto_vcipherlast(x,x);
 	x=__builtin_crypto_vncipher(x,x);
