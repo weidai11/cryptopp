709ca5c480f2394655934907c6b52d3a2e542669
Jeffrey Walton <noloader@gmail.com>
2018-12-09 09:39:38 -0500

    Update Travis Android SDK and NDK gear

--- TestScripts/setenv-travis.sh
+++ TestScripts/setenv-travis.sh
@@ -1,7 +1,13 @@
 #/usr/bin/env bash
 
+# This file should be source'd when required.
+
 export ANDROID_HOME="$HOME/.android"
 export ANDROID_SDK="$HOME/android/sdk/"
 export ANDROID_NDK="$HOME/android/sdk/ndk-bundle"
 export ANDROID_SDK_ROOT="$ANDROID_SDK"
 export ANDROID_NDK_ROOT="$ANDROID_NDK"
+
+mkdir -p "$ANDROID_HOME"
+mkdir -p "$ANDROID_SDK_ROOT"
+mkdir -p "$ANDROID_NDK_ROOT"
