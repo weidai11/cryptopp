7a191ba42d86fcb755e8253af053bc3691a0f2a5
Jeffrey Walton <noloader@gmail.com>
2018-12-09 04:51:07 -0500

    Update Travis Android SDK and NDK gear

--- TestScripts/install-android.sh
+++ TestScripts/install-android.sh
@@ -5,13 +5,14 @@ set -e
 sudo apt-get -qq update
 sudo apt-get -qq install --no-install-recommends openjdk-8-jdk unzip
 
-# Required directories
-mkdir =p "$HOME/android"
+# Required directories, set in the environment
 mkdir -p "$ANDROID_HOME"
+mkdir =p "$ANDROID_SDK_ROOT"
+mkdir =p "$ANDROID_NDK_ROOT"
 
 # android skd/ndk
 curl -Lo /tmp/android-sdk.zip https://dl.google.com/android/repository/sdk-tools-linux-3859397.zip
-unzip -qq /tmp/android-sdk.zip -d "$ANDROID_SDK/"
+unzip -qq /tmp/android-sdk.zip -d "$ANDROID_SDK"
 rm -f /tmp/android-sdk.zip
 echo y | "$ANDROID_SDK/tools/bin/sdkmanager" --update > /dev/null
 for package in "ndk-bundle"; do
