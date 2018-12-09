fbb9b403973ea93017ac24205a20b8f96a9be00e
Jeffrey Walton <noloader@gmail.com>
2018-12-09 09:43:15 -0500

    Update Travis Android SDK and NDK gear

--- TestScripts/install-android.sh
+++ TestScripts/install-android.sh
@@ -1,30 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-# for local debugging
-if [[ -f ./setenv-travis.sh ]]; then
-	echo "Setting variables in setenv-travis.sh"
-    source ./setenv-travis.sh
-elif [[ -f ./TestScripts/setenv-travis.sh ]]; then
-	echo "Setting variables in TestScripts/setenv-travis.sh"
-    source ./TestScripts/setenv-travis.sh
-fi
-
-if [[ -z "$ANDROID_SDK_ROOT" ]] || [[ -z "$ANDROID_NDK_ROOT" ]]; then
-	echo "ANDROID_SDK_ROOT or ANDROID_NDK_ROOT is not set"
-fi
-
 # install android deps
 sudo apt-get -qq update
 sudo apt-get -qq install --no-install-recommends openjdk-8-jdk unzip
-
-# Required directories, set in the environment
-mkdir -p "$ANDROID_HOME"
-mkdir -p "$ANDROID_SDK_ROOT"
-mkdir -p "$ANDROID_NDK_ROOT"
-
-# https://stackoverflow.com/a/47028911/608639
-touch "$ANDROID_HOME/repositories.cfg"
 
 # android skd/ndk
 curl -Lo /tmp/android-sdk.zip https://dl.google.com/android/repository/sdk-tools-linux-3859397.zip
--- TestScripts/setenv-travis.sh
+++ TestScripts/setenv-travis.sh
@@ -11,3 +11,6 @@ export ANDROID_NDK_ROOT="$ANDROID_NDK"
 mkdir -p "$ANDROID_HOME"
 mkdir -p "$ANDROID_SDK_ROOT"
 mkdir -p "$ANDROID_NDK_ROOT"
+
+# https://stackoverflow.com/a/47028911/608639
+touch "$ANDROID_HOME/repositories.cfg"
