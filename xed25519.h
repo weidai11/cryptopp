0311daf7e20922ca628af99187d1469f7411370e
Jeffrey Walton <noloader@gmail.com>
2018-12-25 16:18:01 -0500

    Update documentation

--- xed25519.h
+++ xed25519.h
@@ -320,12 +320,14 @@ protected:
 ///   prime and binary fields.
 /// \details ed25519PrivateKey is also unusual because the
 ///   class members of interest are byte arrays and not Integers.
-///   In addition, the byte byte arrays are "little-endian" meaning
+///   In addition, the byte arrays are little-endian meaning
 ///   LSB is at element 0 and the MSB is at element 31.
-/// \details If you call GetPrivateExponent() then the little-endian
-///   byte array is converted to a big-endian Integer so it can be
-///   returned the way a caller expects. And calling
-///   SetPrivateExponent() perfoms a similar internal conversion.
+///   If you call \ref ed25519PrivateKey::GetPrivateExponent()
+///   "GetPrivateExponent()" then the little-endian byte array is
+///   converted to a big-endian Integer() so it can be returned
+///   the way a caller expects. And calling
+///   \ref ed25519PrivateKey::SetPrivateExponent "SetPrivateExponent()"
+///   perfoms a similar internal conversion.
 /// \since Crypto++ 8.0
 struct ed25519PrivateKey : public PKCS8PrivateKey
 {
@@ -575,12 +577,14 @@ protected:
 ///   prime and binary fields.
 /// \details ed25519PublicKey is also unusual because the
 ///   class members of interest are byte arrays and not Integers.
-///   In addition, the byte byte arrays are "little-endian" meaning
+///   In addition, the byte arrays are little-endian meaning
 ///   LSB is at element 0 and the MSB is at element 31.
-/// \details If you call GetPublicElement() then the little-endian
-///   byte array is converted to a big-endian Integer so it can be
-///   returned the way a caller expects. And calling
-///   SetPublicElement() perfoms a similar internal conversion.
+///   If you call \ref ed25519PublicKey::GetPublicElement()
+///   "GetPublicElement()" then the little-endian byte array is
+///   converted to a big-endian Integer() so it can be returned
+///   the way a caller expects. And calling
+///   \ref ed25519PublicKey::SetPublicElement "SetPublicElement()"
+///   perfoms a similar internal conversion.
 /// \since Crypto++ 8.0
 struct ed25519PublicKey : public X509PublicKey
 {
