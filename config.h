2c8bec02c1af01fab42da99b4d123f54ba6bfa62
Jeffrey Walton <noloader@gmail.com>
2019-04-27 18:08:31 -0400

    Update config.h We should not need the workarounds by switching to Power8

--- config.h
+++ config.h
@@ -74,24 +74,6 @@
 // Also see https://bugs.llvm.org/show_bug.cgi?id=39895 .
 // #define CRYPTOPP_DISABLE_MIXED_ASM 1
 
-// Several compilers discard SIMD code that loads unaligned data. The symptom
-// is often self test failures and UBsan findings for unaligned loads. For
-// example, Power7 can load unaligned data using vec_vsx_ld but some versions
-// of GCC and Clang require 16-byte aligned data when using the builtin.
-// It is not limited to SSE and PowerPC code. Define this to disable
-// Crypto++ code that uses potentially problematic builtins or intrinsics.
-// Also see https://gcc.gnu.org/bugzilla/show_bug.cgi?id=88234 and
-// https://bugs.llvm.org/show_bug.cgi?id=39704
-// #define CRYPTOPP_BUGGY_SIMD_LOAD_AND_STORE 1
-
-// This list will probably grow over time as more compilers are identified.
-#if defined(CRYPTOPP_BUGGY_SIMD_LOAD_AND_STORE)
-# define CRYPTOPP_DISABLE_LEA_SIMD 1
-# define CRYPTOPP_DISABLE_SIMON_SIMD 1
-# define CRYPTOPP_DISABLE_SPECK_SIMD 1
-# define CRYPTOPP_DISABLE_SM4_SIMD 1
-#endif
-
 // Define CRYPTOPP_NO_CXX11 to avoid C++11 related features shown at the
 // end of this file. Some compilers and standard C++ headers advertise C++11
 // but they are really just C++03 with some additional C++11 headers and
