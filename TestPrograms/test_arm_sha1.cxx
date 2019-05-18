f027361369259d4d1dabc2d3e373d0e109f047fd
Jeffrey Walton <noloader@gmail.com>
2019-05-17 22:19:26 -0400

    Split test_arm_sha.cxx into SHA1 and SHA256 tests

---
+++ TestPrograms/test_arm_sha1.cxx
@@ -0,0 +1,18 @@
+#include <arm_neon.h>
+#include <stdint.h>
+
+// test_acle.h determines if this is available. Then,
+// -DCRYPTOPP_ARM_ACLE_AVAILABLE=0 is added to CXXFLAGS
+// if the ACLE header is not available.
+#if (CRYPTOPP_ARM_ACLE_AVAILABLE)
+#  include <arm_acle.h>
+#endif
+
+int main(int argc, char* argv[])
+{
+	uint32x4_t y = {0};
+	y=vsha1cq_u32(y,0,y);
+	y=vsha1mq_u32(y,1,y);
+	y=vsha1pq_u32(y,2,y);
+	return 0;
+}
--- TestPrograms/test_arm_sha256.cxx
+++ TestPrograms/test_arm_sha256.cxx
@@ -11,9 +11,6 @@
 int main(int argc, char* argv[])
 {
 	uint32x4_t y = {0};
-	y=vsha1cq_u32(y,0,y);
-	y=vsha1mq_u32(y,1,y);
-	y=vsha1pq_u32(y,2,y);
 	y=vsha256hq_u32(y, y, y);
 	y=vsha256h2q_u32(y, y, y);
 	y=vsha256su1q_u32(y, y, y);
