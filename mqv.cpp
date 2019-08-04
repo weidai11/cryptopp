9be8c7074c4a442442e426b6c67194bfaa5eefa8
Jeffrey Walton <noloader@gmail.com>
2019-08-04 16:51:17 -0400

    Add test instantiations for ECMQV, ECHMQV and ECFHMQV

--- mqv.cpp
+++ mqv.cpp
@@ -7,6 +7,7 @@
 #include "mqv.h"
 #include "hmqv.h"
 #include "fhmqv.h"
+#include "eccrypto.h"
 
 // Squash MS LNK4221 and libtool warnings
 extern const char MQV_FNAME[] = __FILE__;
@@ -16,17 +17,29 @@ NAMESPACE_BEGIN(CryptoPP)
 #if defined(CRYPTOPP_DEBUG) && !defined(CRYPTOPP_DOXYGEN_PROCESSING)
 void TestInstantiations_MQV()
 {
-	MQV mqv;
+    MQV mqv;
+    ECMQV<ECP> ecmqv;
+
+    CRYPTOPP_UNUSED(mqv);
+    CRYPTOPP_UNUSED(ecmqv);
 }
 
 void TestInstantiations_HMQV()
 {
     HMQV hmqv;
+    ECHMQV<ECP> echmqv;
+
+    CRYPTOPP_UNUSED(hmqv);
+    CRYPTOPP_UNUSED(echmqv);
 }
 
 void TestInstantiations_FHMQV()
 {
     FHMQV fhmqv;
+    ECFHMQV<ECP> ecfhmqv;
+
+    CRYPTOPP_UNUSED(fhmqv);
+    CRYPTOPP_UNUSED(ecfhmqv);
 }
 #endif
 
