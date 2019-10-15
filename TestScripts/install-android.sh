d8c6f63680cf8852227f63e4728ed675caae7e9e
Jeffrey Walton <noloader@gmail.com>
2019-10-15 11:41:39 -0400

    Update install-android.sh script

--- TestScripts/install-android.sh
+++ TestScripts/install-android.sh
@@ -6,11 +6,10 @@ sudo apt-get -qq update
 sudo apt-get -qq install --no-install-recommends openjdk-8-jdk unzip
 
 # android skd/ndk
-curl -Lo /tmp/android-sdk.zip https://dl.google.com/android/repository/sdk-tools-linux-3859397.zip
+curl -L -k -o /tmp/android-sdk.zip https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip
 unzip -qq /tmp/android-sdk.zip -d "$ANDROID_SDK"
 rm -f /tmp/android-sdk.zip
-echo y | "$ANDROID_SDK/tools/bin/sdkmanager" --update > /dev/null
-for package in "ndk-bundle"; do
-	echo install android $package
-	echo y | "$ANDROID_SDK/tools/bin/sdkmanager" "$package" > /dev/null
-done
+
+curl -L -k -o /tmp/android-ndk.zip https://dl.google.com/android/repository/android-ndk-r19c-linux-x86_64.zip
+unzip -qq /tmp/android-sdk.zip -d "$ANDROID_NDK"
+rm -f /tmp/android-ndk.zip
