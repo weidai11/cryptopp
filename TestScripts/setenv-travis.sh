02fe84b6b4ddf38c7da5df6508ede95c2ccd62c4
Jeffrey Walton <noloader@gmail.com>
2019-10-15 14:37:59 -0400

    Dump toolchain locations

--- TestScripts/setenv-travis.sh
+++ TestScripts/setenv-travis.sh
@@ -7,7 +7,7 @@ echo "Setting Travis environment"
 #export ANDROID_HOME="$HOME/.android"
 export ANDROID_HOME="$HOME/android/sdk/"
 export ANDROID_SDK="$HOME/android/sdk/"
-export ANDROID_NDK="$HOME/android/sdk/ndk-bundle"
+export ANDROID_NDK="$HOME/android/ndk/"
 export ANDROID_SDK_ROOT="$ANDROID_SDK"
 export ANDROID_NDK_ROOT="$ANDROID_NDK"
 
