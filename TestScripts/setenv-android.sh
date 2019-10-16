1fe72917d953961771ba178523d979c7163e56a7
Jeffrey Walton <noloader@gmail.com>
2019-10-16 02:45:45 -0400

    Test enable NEON for Android

--- TestScripts/setenv-android.sh
+++ TestScripts/setenv-android.sh
@@ -156,7 +156,7 @@ case "$THE_ARCH" in
     RANLIB="arm-linux-androideabi-ranlib"
     STRIP="arm-linux-androideabi-strip"
 
-    AOSP_FLAGS="-march=armv7-a -mthumb -mfpu=vfpv3-d16 -mfloat-abi=softfp -DCRYPTOPP_DISABLE_ASM -funwind-tables -fexceptions -frtti"
+    AOSP_FLAGS="-march=armv7-a -mthumb -mfpu=vfpv3-d16 -mfloat-abi=softfp -funwind-tables -fexceptions -frtti"
     ;;
   armv8|armv8a|aarch64|arm64|arm64-v8a)
     CC="aarch64-linux-android$AOSP_API-clang"
