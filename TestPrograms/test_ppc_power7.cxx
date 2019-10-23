aeadfcfc5b482b52f1d43af386c0953914483aca
Jeffrey Walton <noloader@gmail.com>
2019-10-23 13:27:56 -0400

    Add another workaround for Clang on PowerPC

--- TestPrograms/test_ppc_power7.cxx
+++ TestPrograms/test_ppc_power7.cxx
@@ -3,7 +3,7 @@ int main(int argc, char* argv[])
 {
 #if defined(_ARCH_PWR7)
     __vector unsigned long long z = {1, 2};
-    z=vec_add(z, vec_xl(0, (unsigned long long*)argv[0]));
+    z=vec_add(z, vec_xl(0, (__vector unsigned long long*)argv[0]));
 #else
     int x[-1];
 #endif
