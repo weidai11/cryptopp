38f658682200e9c18fd90473d144ed6d4cc67248
Jeffrey Walton <noloader@gmail.com>
2019-10-23 16:00:47 -0400

    Update feature tests

--- TestPrograms/test_ppc_power7.cxx
+++ TestPrograms/test_ppc_power7.cxx
@@ -2,8 +2,13 @@
 int main(int argc, char* argv[])
 {
 #if defined(_ARCH_PWR7)
-    __vector unsigned long long z = {1, 2};
-    z=vec_add(z, vec_xl(0, (__vector unsigned long long*)argv[0]));
+    __vector unsigned int x = {1, 2};
+    __vector unsigned int y = vec_add(x, vec_ld(0, (__vector unsigned int*)argv[0]));
+#  if defined(__VSX__)
+	__vector unsigned long long xx = (__vector unsigned long long)x;
+	__vector unsigned long long yy = (__vector unsigned long long)y;
+	__vector unsigned long long zz = vec_add(xx, yy);
+#  endif
 #else
     int x[-1];
 #endif
