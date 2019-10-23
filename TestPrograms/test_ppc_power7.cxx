3662355b9905ef31a988d69150c47b18099759ec
Jeffrey Walton <noloader@gmail.com>
2019-10-23 16:33:21 -0400

    Whitespace check-in

--- TestPrograms/test_ppc_power7.cxx
+++ TestPrograms/test_ppc_power7.cxx
@@ -4,6 +4,7 @@ int main(int argc, char* argv[])
 #if defined(_ARCH_PWR7)
     __vector unsigned int x = {1, 2};
     __vector unsigned int y = vec_add(x, vec_ld(0, (__vector unsigned int*)argv[0]));
+	__vector unsigned int z = vec_xor(x, y);
 #  if defined(__VSX__)
 	__vector unsigned long long xx = (__vector unsigned long long)x;
 	__vector unsigned long long yy = (__vector unsigned long long)y;
