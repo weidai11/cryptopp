e014694fe3b0ad58ff4a944ca748e901da709101
Jeffrey Walton <noloader@gmail.com>
2020-07-09 19:50:38 -0400

    Update cryptest-autotools.sh script

--- TestScripts/cryptest-autotools.sh
+++ TestScripts/cryptest-autotools.sh
@@ -14,43 +14,43 @@ AWK=awk
 MAKE=make
 
 # Fixup, Solaris and friends
-if [[ (-d /usr/xpg4/bin) ]]; then
+if [[ -d /usr/xpg4/bin ]]; then
 	SED=/usr/xpg4/bin/sed
 	AWK=/usr/xpg4/bin/awk
 	GREP=/usr/xpg4/bin/grep
-elif [[ (-d /usr/bin/posix) ]]; then
+elif [[ -d /usr/bin/posix ]]; then
 	SED=/usr/bin/posix/sed
 	AWK=/usr/bin/posix/awk
 	GREP=/usr/bin/posix/grep
 fi
 
 # Fixup for sed and "illegal byte sequence"
-IS_DARWIN=$(uname -s | "$GREP" -i -c darwin)
+IS_DARWIN=$(uname -s 2>/dev/null | "$GREP" -i -c darwin)
 if [[ "$IS_DARWIN" -ne 0 ]]; then
 	export LC_ALL=C
 fi
 
 # Fixup for Solaris and BSDs
-if [[ ! -z $(command -v gmake) ]]; then
+if [[ ! -z $(command -v gmake 2>/dev/null) ]]; then
 	MAKE=gmake
 else
 	MAKE=make
 fi
 
 # Fixup for missing libtool
-if [[ ! -z $(command -v libtoolize) ]]; then
-	export LIBTOOLIZE=$(command -v libtoolize)
-elif [[ ! -z $(command -v glibtoolize) ]]; then
+if [[ ! -z $(command -v glibtoolize 2>/dev/null) ]]; then
 	export LIBTOOLIZE=$(command -v glibtoolize)
-elif [[ ! -z $(command -v libtool) ]]; then
-	export LIBTOOLIZE=$(command -v libtool)
-elif [[ ! -z $(command -v glibtool) ]]; then
+elif [[ ! -z $(command -v libtoolize 2>/dev/null) ]]; then
+	export LIBTOOLIZE=$(command -v libtoolize)
+elif [[ ! -z $(command -v glibtool 2>/dev/null) ]]; then
 	export LIBTOOLIZE=$(command -v glibtool)
+elif [[ ! -z $(command -v libtool 2>/dev/null) ]]; then
+	export LIBTOOLIZE=$(command -v libtool)
 fi
 
 #############################################################################
 
-if [[ -z $(command -v autoupdate) ]]; then
+if [[ -z $(command -v autoupdate 2>/dev/null) ]]; then
 	echo "Cannot find autoupdate. Things may fail."
 fi
 
@@ -58,15 +58,15 @@ if [[ -z "$LIBTOOLIZE" ]]; then
 	echo "Cannot find libtoolize. Things may fail."
 fi
 
-if [[ -z $(command -v automake) ]]; then
+if [[ -z $(command -v automake 2>/dev/null) ]]; then
 	echo "Cannot find automake. Things may fail."
 fi
 
-if [[ -z $(command -v autoreconf) ]]; then
+if [[ -z $(command -v autoreconf 2>/dev/null) ]]; then
 	echo "Cannot find autoreconf. Things may fail."
 fi
 
-if [[ -z $(command -v curl) ]]; then
+if [[ -z $(command -v curl 2>/dev/null) ]]; then
 	echo "Cannot find cURL. Things may fail."
 fi
 
@@ -114,7 +114,7 @@ chmod +w build-aux/config.sub
 mv config.sub.new build-aux/config.sub
 chmod +x build-aux/config.sub
 
-if [[ "$IS_DARWIN" -ne 0 ]] && [[ -n $(command -v xattr) ]]; then
+if [[ "$IS_DARWIN" -ne 0 ]] && [[ -n $(command -v xattr 2>/dev/null) ]]; then
 	echo "Removing config.sub quarantine"
 	xattr -d "com.apple.quarantine" build-aux/config.sub &>/dev/null
 fi
@@ -127,7 +127,7 @@ chmod +w build-aux/config.guess
 mv config.guess.new build-aux/config.guess
 chmod +x build-aux/config.guess
 
-if [[ "$IS_DARWIN" -ne 0 ]] && [[ -n $(command -v xattr) ]]; then
+if [[ "$IS_DARWIN" -ne 0 ]] && [[ -n $(command -v xattr 2>/dev/null) ]]; then
 	echo "Removing config.guess quarantine"
 	xattr -d "com.apple.quarantine" build-aux/config.guess &>/dev/null
 fi
