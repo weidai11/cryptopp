1432ae08195be0f5c797760fb987b568ceb715b9
Jeffrey Walton <noloader@gmail.com>
2020-07-27 11:55:24 -0400

    Add additiona; test programs

---
+++ TestPrograms/test_x86_via_rng.cxx
@@ -0,0 +1,38 @@
+#include <cstdlib>
+int main(int argc, char* argv[])
+{
+	unsigned int msr=0;
+	unsigned int divisor=2;
+    unsigned int buffer;
+
+	__asm__ __volatile__
+	(
+#if (CRYPTOPP_BOOL_X32 || CRYPTOPP_BOOL_X64)
+		"mov  %1, %%rdi          ;\n"
+		"movl %2, %%edx          ;\n"
+#else
+		"mov  %1, %%edi          ;\n"
+		"movl %2, %%edx          ;\n"
+#endif
+
+		// xstore-rng
+		".byte 0x0f, 0xa7, 0xc0  ;\n"
+
+#if (CRYPTOPP_BOOL_X32 || CRYPTOPP_BOOL_X64)
+		"andq %%rax, 0x1f        ;\n"
+		"movl %%eax, %0          ;\n"
+#else
+		"andl  %%eax, 0x1f       ;\n"
+		"movl  %%eax, %0         ;\n"
+#endif
+
+		: "=g" (msr) : "g" (buffer), "g" (divisor)
+#if (CRYPTOPP_BOOL_X32 || CRYPTOPP_BOOL_X64)
+		: "rax", "rdx", "rdi", "cc"
+#else
+		: "eax", "edx", "edi", "cc"
+#endif
+	);
+
+	return 0;
+}
