0d85286410202b559d0f6156041eb991089b46d6
Jeffrey Walton <noloader@gmail.com>
2020-08-06 04:18:38 -0400

    Update comments

--- TestPrograms/test_cxx11_dyninit.cxx
+++ TestPrograms/test_cxx11_dyninit.cxx
@@ -1,4 +1,5 @@
 // https://en.cppreference.com/w/cpp/feature_test
+// Apple bug https://bugs.llvm.org/show_bug.cgi?id=47012.
 int main(int argc, char* argv[])
 {
 #if __cpp_threadsafe_static_init >= 200806L
