3ca143b1900dbb24699dde563ea0ebfb46bbefbf
Jeffrey Walton <noloader@gmail.com>
2020-08-06 17:45:35 -0400

    Fix warning suppression on Apple platforms

--- TestPrograms/test_ppc_power8.cxx
+++ TestPrograms/test_ppc_power8.cxx
@@ -1,5 +1,12 @@
 #define GNUC_VERSION (__GNUC__*1000 + __GNUC_MAJOR__*10)
-#if (GNUC_VERSION >= 4060) || defined(__clang__)
+
+#if defined(__apple_build_version__)
+# define APPLE_VERSION (__clang__*1000 + __clang_minor__*10)
+#else
+# define LLVM_VERSION (__clang__*1000 + __clang_minor__*10)
+#endif
+
+#if (GNUC_VERSION >= 4060) || (LLVM_VERSION >= 1070) || (APPLE_VERSION >= 2000)
 # pragma GCC diagnostic ignored "-Wdeprecated"
 #endif
 
