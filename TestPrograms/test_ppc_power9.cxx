5334683711cf3c1f606e7f3e459d40d9eef3f44d
Jeffrey Walton <noloader@gmail.com>
2020-08-06 17:49:39 -0400

    Use __clang_major__ rather than __clang__ in macro

--- TestPrograms/test_ppc_power9.cxx
+++ TestPrograms/test_ppc_power9.cxx
@@ -1,9 +1,9 @@
 #define GNUC_VERSION (__GNUC__*1000 + __GNUC_MAJOR__*10)
 
 #if defined(__apple_build_version__)
-# define APPLE_VERSION (__clang__*1000 + __clang_minor__*10)
+# define APPLE_VERSION (__clang_major__*1000 + __clang_minor__*10)
 #else
-# define LLVM_VERSION (__clang__*1000 + __clang_minor__*10)
+# define LLVM_VERSION (__clang_major__*1000 + __clang_minor__*10)
 #endif
 
 #if (GNUC_VERSION >= 4060) || (LLVM_VERSION >= 1070) || (APPLE_VERSION >= 2000)
