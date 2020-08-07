31036ca0568d8eceba07bd98a00f647baa30d582
Jeffrey Walton <noloader@gmail.com>
2020-08-07 01:12:27 -0400

    Use __clang__ preprocessor macro in tests

--- TestPrograms/test_cxx11_noexcept.cxx
+++ TestPrograms/test_cxx11_noexcept.cxx
@@ -2,15 +2,15 @@
 # define GNUC_VERSION (__GNUC__*1000 + __GNUC_MINOR__*10)
 #endif
 
-#if defined(__clang_major__) && defined(__apple_build_version__)
+#if defined(__clang__) && defined(__apple_build_version__)
 # undef GNUC_VERSION
 # define APPLE_VERSION (__clang_major__*1000 + __clang_minor__*10)
-#elif defined(__clang_major__)
+#elif defined(__clang__)
 # undef GNUC_VERSION
 # define LLVM_VERSION (__clang_major__*1000 + __clang_minor__*10)
 #endif
 
-#if (GNUC_VERSION >= 4060) || (LLVM_VERSION >= 9000) || (APPLE_VERSION >= 10000)
+#if (GNUC_VERSION >= 7030)
 # pragma GCC diagnostic ignored "-Wterminate"
 #endif
 
--- TestPrograms/test_ppc_power9.cxx
+++ TestPrograms/test_ppc_power9.cxx
@@ -2,10 +2,10 @@
 # define GNUC_VERSION (__GNUC__*1000 + __GNUC_MINOR__*10)
 #endif
 
-#if defined(__clang_major__) && defined(__apple_build_version__)
+#if defined(__clang__) && defined(__apple_build_version__)
 # undef GNUC_VERSION
 # define APPLE_VERSION (__clang_major__*1000 + __clang_minor__*10)
-#elif defined(__clang_major__)
+#elif defined(__clang__)
 # undef GNUC_VERSION
 # define LLVM_VERSION (__clang_major__*1000 + __clang_minor__*10)
 #endif
