47b38d8a6bcbb63bf0b9ad520198bb8eab2b8241
Jeffrey Walton <noloader@gmail.com>
2020-08-06 21:11:00 -0400

    Fix warning suppression macros in test programs

--- TestPrograms/test_ppc_power9.cxx
+++ TestPrograms/test_ppc_power9.cxx
@@ -1,10 +1,12 @@
-#if defined(__GNUC_MAJOR__)
-# define GNUC_VERSION (__GNUC__*1000 + __GNUC_MAJOR__*10)
+#if defined(__GNUC__)
+# define GNUC_VERSION (__GNUC__*1000 + __GNUC_MINOR__*10)
 #endif
 
 #if defined(__clang_major__) && defined(__apple_build_version__)
+# undef GNUC_VERSION
 # define APPLE_VERSION (__clang_major__*1000 + __clang_minor__*10)
 #elif defined(__clang_major__)
+# undef GNUC_VERSION
 # define LLVM_VERSION (__clang_major__*1000 + __clang_minor__*10)
 #endif
 
