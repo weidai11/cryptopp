6f1aa13d22897330a0a53e1ff5fcade12c82d042
Jeffrey Walton <noloader@gmail.com>
2020-11-29 18:43:16 -0500

    Fix CHAM64 AlgorithmProvider We removed the template based SIMD code for 64-bit algorithms due to incorrect results for CFB and OFB modes

--- cham.cpp
+++ cham.cpp
@@ -114,12 +114,6 @@ extern size_t CHAM128_Dec_AdvancedProcessBlocks_SSSE3(const word32* subKeys, siz
 
 std::string CHAM64::Base::AlgorithmProvider() const
 {
-#if (CRYPTOPP_CHAM128_ADVANCED_PROCESS_BLOCKS)
-# if defined(CRYPTOPP_SSSE3_AVAILABLE)
-    if (HasSSSE3())
-        return "SSSE3";
-# endif
-#endif
     return "C++";
 }
 
