16db9fa57b3573c62f1ad9070c95f6be05b59f13
Jeffrey Walton <noloader@gmail.com>
2020-12-15 02:46:46 -0500

    Update documentation

--- hrtimer.h
+++ hrtimer.h
@@ -1,3 +1,8 @@
+// hrtimer.h - originally written and placed in the public domain by Wei Dai
+
+/// \file hrtimer.h
+/// \brief Classes for timers
+
 #ifndef CRYPTOPP_HRTIMER_H
 #define CRYPTOPP_HRTIMER_H
 
@@ -16,19 +21,42 @@ NAMESPACE_BEGIN(CryptoPP)
 #endif
 
 /// \brief Base class for timers
+/// \sa ThreadUserTimer, Timer
 class CRYPTOPP_DLL CRYPTOPP_NO_VTABLE TimerBase
 {
 public:
+	/// \brief Unit of measure
 	enum Unit {SECONDS = 0, MILLISECONDS, MICROSECONDS, NANOSECONDS};
+
+	/// \brief Construct a TimerBase
+	/// \param unit the unit of measure
+	/// \param stuckAtZero flag
 	TimerBase(Unit unit, bool stuckAtZero)
 		: m_timerUnit(unit), m_stuckAtZero(stuckAtZero), m_started(false)
 		, m_start(0), m_last(0) {}
 
-	virtual TimerWord GetCurrentTimerValue() =0;	// GetCurrentTime is a macro in MSVC 6.0
-	virtual TimerWord TicksPerSecond() =0;	// this is not the resolution, just a conversion factor into seconds
+	/// \brief Retrieve the current timer value
+	/// \return the current timer value
+	virtual TimerWord GetCurrentTimerValue() =0;
 
+	/// \brief Retrieve ticks per second
+	/// \return ticks per second
+	/// \details TicksPerSecond() is not the timer resolution. It is a
+	///  conversion factor into seconds.
+	virtual TimerWord TicksPerSecond() =0;
+
+	/// \brief Start the timer
+	/// \return the current timer value
 	void StartTimer();
+
+	/// \brief Retrieve the elapsed time
+	/// \return the elapsed time as a double
+	/// \sa ElapsedTime
 	double ElapsedTimeAsDouble();
+
+	/// \brief Retrieve the elapsed time
+	/// \return the elapsed time as an unsigned long
+	/// \sa ElapsedTimeAsDouble
 	unsigned long ElapsedTime();
 
 private:
@@ -39,22 +67,34 @@ private:
 	TimerWord m_start, m_last;
 };
 
-/// \brief Measure CPU time spent executing instructions of this thread (if supported by OS)
-/// \note ThreadUserTimer only works correctly on Windows NT or later desktops and servers.
-/// On Unix-based it reports process time. On Windows Phone and Windows Store it reports wall
-/// clock time with performance counter precision. On all others it reports wall clock time.
+/// \brief Measure CPU time spent executing instructions of this thread
+/// \details ThreadUserTimer requires support of the OS. On Unix-based it
+///  reports process time. On Windows NT or later desktops and servers it
+///  reports thread times with performance counter precision.. On Windows
+///  Phone and Windows Store it reports wall clock time with performance
+///  counter precision. On all others it reports wall clock time.
+/// \note ThreadUserTimer only works correctly on Windows NT or later
+///  desktops and servers.
+/// \sa Timer
 class ThreadUserTimer : public TimerBase
 {
 public:
+	/// \brief Construct a ThreadUserTimer
+	/// \param unit the unit of measure
+	/// \param stuckAtZero flag
 	ThreadUserTimer(Unit unit = TimerBase::SECONDS, bool stuckAtZero = false) : TimerBase(unit, stuckAtZero) {}
 	TimerWord GetCurrentTimerValue();
 	TimerWord TicksPerSecond();
 };
 
-/// high resolution timer
+/// \brief High resolution timer
+/// \sa ThreadUserTimer
 class CRYPTOPP_DLL Timer : public TimerBase
 {
 public:
+	/// \brief Construct a Timer
+	/// \param unit the unit of measure
+	/// \param stuckAtZero flag
 	Timer(Unit unit = TimerBase::SECONDS, bool stuckAtZero = false)	: TimerBase(unit, stuckAtZero) {}
 	TimerWord GetCurrentTimerValue();
 	TimerWord TicksPerSecond();
