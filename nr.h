fe4f9a0748e2d7250bbf4ccc5bf386035208b0a8
Jeffrey Walton <noloader@gmail.com>
2020-12-15 03:52:56 -0500

    Update documentation

--- nr.h
+++ nr.h
@@ -1,3 +1,8 @@
+// nr.h - originally written and placed in the public domain by Wei Dai
+
+/// \file nr.h
+/// \brief Classes for Nyberg-Rueppel signature scheme
+
 #ifndef CRYPTOPP_NR_H
 #define CRYPTOPP_NR_H
 
