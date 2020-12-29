9fe5ccfbeed3c3c48b6e1d42e4abb64d11662527
Jeffrey Walton <noloader@gmail.com>
2020-12-28 21:32:29 -0500

    Revert e235ac57e85b (GH #992) These damn aligned arrays are going to be the death of us...

--- misc.h
+++ misc.h
@@ -1212,15 +1212,14 @@ inline bool IsAlignedOn(const void *ptr, unsigned int alignment)
 /// \brief Determines whether ptr is minimally aligned
 /// \tparam T class or type
 /// \param ptr the pointer to check for alignment
-/// \return true if <tt>ptr</tt> is aligned to at least a pointer to <tt>T</tt>
+/// \return true if <tt>ptr</tt> is aligned to at least <tt>T</tt>
 ///  boundary, false otherwise
 /// \details Internally the function calls IsAlignedOn with a second
-///  parameter of GetAlignmentOf<T*>.
+///  parameter of GetAlignmentOf<T>.
 template <class T>
 inline bool IsAligned(const void *ptr)
 {
-	// Switch to T* due to https://github.com/weidai11/cryptopp/issues/992
-	return IsAlignedOn(ptr, GetAlignmentOf<T*>());
+	return IsAlignedOn(ptr, GetAlignmentOf<T>());
 }
 
 #if (CRYPTOPP_LITTLE_ENDIAN)
