b94ef052702bf94dda7d762d97c12f5a46bd9d99
Jeffrey Walton <noloader@gmail.com>
2021-03-10 11:51:03 -0500

    Add setenv-macos.sh script

---
+++ TestScripts/setenv-macos.sh
@@ -0,0 +1,301 @@
+#!/usr/bin/env bash
+
+#############################################################################
+#
+# This script sets the cross-compile environment for Xcode/MacOS.
+#
+# Based upon OpenSSL's setenv-android.sh by TH, JW, and SM.
+# Heavily modified by JWW for Crypto++.
+# Modified some more by JW and UB.
+#
+# Crypto++ Library is copyrighted as a compilation and (as of version 5.6.2)
+# licensed under the Boost Software License 1.0, while the individual files
+# in the compilation are all public domain.
+#
+# See http://www.cryptopp.com/wiki/MacOS_(Command_Line) for more details
+#############################################################################
+
+#########################################
+#####        Some validation        #####
+#########################################
+
+# In the past we could mostly infer arch or cpu from the SDK (and mostly
+# vice-versa). Nowadays we need the user to set it for us because Apple
+# platforms have both 32-bit or 64-bit variations.
+
+# cryptest-macos.sh may run this script without sourcing.
+if [ "$0" = "${BASH_SOURCE[0]}" ]; then
+    echo "setenv-macos.sh is usually sourced, but not this time."
+fi
+
+# This is fixed since we are building for MacOS
+MACOS_SDK=MacOSX
+
+# This supports 'source setenv-macos.sh iPhone arm64' and
+# 'source setenv-macos.sh MACOS_CPU=arm64'
+if [[ -n "$1" ]]
+then
+    arg1=$(echo "$1" | cut -f 1 -d '=')
+    arg2=$(echo "$1" | cut -f 2 -d '=')
+    if [[ -n "${arg2}" ]]; then
+        MACOS_CPU="${arg2}"
+    else
+        MACOS_CPU="${arg1}"
+    fi
+    printf "Using positional arg, MACOS_CPU=%s\n" "${MACOS_CPU}"
+fi
+
+# Sane default. Use current machine.
+if [ -z "$MACOS_CPU" ]; then
+    MACOS_CPU="$(uname -m 2>/dev/null)"
+    if [[ "$MACOS_CPU" == "Power Macintosh" ]] ; then
+        MACOS_CPU="ppc"
+    fi
+fi
+
+if [ -z "$MACOS_CPU" ]; then
+    echo "MACOS_CPU is not set. Please set it"
+    [ "$0" = "${BASH_SOURCE[0]}" ] && exit 1 || return 1
+fi
+
+DEF_CPPFLAGS="-DNDEBUG"
+DEF_CXXFLAGS="-Wall -g2 -O3 -fPIC"
+DEF_LDFLAGS=""
+
+#########################################
+#####       Clear old options       #####
+#########################################
+
+unset IS_IOS
+unset IS_MACOS
+unset IS_ANDROID
+unset IS_ARM_EMBEDDED
+
+unset MACOS_CPPFLAGS
+unset MACOS_CXXFLAGS
+unset MACOS_LDFLAGS
+unset MACOS_SYSROOT
+
+#########################################
+#####    Small Fixups, if needed    #####
+#########################################
+
+# Old PowerMac
+if [[ "$MACOS_CPU" == "Power Macintosh" ]] ; then
+    MACOS_CPU=ppc
+fi
+
+if [[ "$MACOS_CPU" == "386" || "$MACOS_CPU" == "i686" || "$MACOS_CPU" == "686" ]] ; then
+    MACOS_CPU=i386
+fi
+
+if [[ "$MACOS_CPU" == "amd64" || "$MACOS_CPU" == "x86_64" ]] ; then
+    MACOS_CPU=x86_64
+fi
+
+if [[ "$MACOS_CPU" == "aarch64" || "$MACOS_CPU" == "arm64"* || "$MACOS_CPU" == "armv8"* ]] ; then
+    MACOS_CPU=arm64
+fi
+
+########################################
+#####         Environment          #####
+########################################
+
+# The flags below were tested with Xcode 8 on Travis. If
+# you use downlevel versions of Xcode, then you can push
+# xxx-version-min=n lower. For example, Xcode 7 can use
+# -miphoneos-version-min=5. However, Xcode 7 lacks
+# AppleTVOS and WatchOS support.
+
+# Also see https://github.com/rust-lang/rust/issues/48862
+# and https://developer.apple.com/documentation/bundleresources/information_property_list/minimumosversion
+
+# iPhones can be either 32-bit or 64-bit
+if [[ "$MACOS_CPU" == "ppc" ]]; then
+    MIN_VER=-mmacosx-version-min=10
+    MACOS_STDLIB=libstdc++
+
+# OS X 10.7 minimum due to -stdlib=libc++
+elif [[ "$MACOS_CPU" == "i386" ]]; then
+    MIN_VER=-mmacosx-version-min=10.7
+    MACOS_STDLIB=libc++
+
+elif [[ "$MACOS_CPU" == "x86_64" ]]; then
+    MIN_VER=-mmacosx-version-min=10.7
+    MACOS_STDLIB=libc++
+
+elif [[ "$MACOS_CPU" == "arm64" ]]; then
+    MIN_VER=-mmacosx-version-min=11.0
+    MACOS_STDLIB=libc++
+
+# And the final catch-all
+else
+    echo "MACOS_CPU is not valid. Please fix it"
+    [ "$0" = "${BASH_SOURCE[0]}" ] && exit 1 || return 1
+fi
+
+#####################################################################
+
+# Xcode 6 and below cannot handle -miphonesimulator-version-min
+# Fix it so the simulator will compile as expected. This trick
+# may work on other platforms, but it was not tested.
+
+if [ -n "$(command -v xcodebuild 2>/dev/null)" ]; then
+    # Output of xcodebuild is similar to "Xcode 6.2". The first cut gets
+    # the dotted decimal value. The second cut gets the major version.
+    XCODE_VERSION=$(xcodebuild -version 2>/dev/null | head -n 1 | cut -f2 -d" " | cut -f1 -d".")
+    if [ -z "$XCODE_VERSION" ]; then XCODE_VERSION=100; fi
+
+    if [ "$XCODE_VERSION" -le 6 ]; then
+        MIN_VER="${MIN_VER//iphonesimulator/iphoneos}"
+    fi
+fi
+
+#####################################################################
+
+# Allow a user override? I think we should be doing this. The use case is:
+# move /Applications/Xcode somewhere else for a side-by-side installation.
+if [ -z "${XCODE_DEVELOPER-}" ]; then
+  XCODE_DEVELOPER=$(xcode-select -print-path 2>/dev/null)
+fi
+
+if [ ! -d "$XCODE_DEVELOPER" ]; then
+  echo "ERROR: unable to find XCODE_DEVELOPER directory."
+  [ "$0" = "${BASH_SOURCE[0]}" ] && exit 1 || return 1
+fi
+
+# XCODE_DEVELOPER_SDK is the SDK location.
+XCODE_DEVELOPER_SDK="$XCODE_DEVELOPER/Platforms/$MACOS_SDK.platform"
+
+if [ ! -d "$XCODE_DEVELOPER_SDK" ]; then
+  echo "ERROR: unable to find XCODE_DEVELOPER_SDK directory."
+  echo "       Is the SDK supported by Xcode and installed?"
+  [ "$0" = "${BASH_SOURCE[0]}" ] && exit 1 || return 1
+fi
+
+# XCODE_TOOLCHAIN is the location of the actual compiler tools.
+if [ -d "$XCODE_DEVELOPER/Toolchains/XcodeDefault.xctoolchain/usr/bin/" ]; then
+  XCODE_TOOLCHAIN="$XCODE_DEVELOPER/Toolchains/XcodeDefault.xctoolchain/usr/bin/"
+elif [ -d "$XCODE_DEVELOPER_SDK/Developer/usr/bin/" ]; then
+  XCODE_TOOLCHAIN="$XCODE_DEVELOPER_SDK/Developer/usr/bin/"
+fi
+
+if [ -z "$XCODE_TOOLCHAIN" ] || [ ! -d "$XCODE_TOOLCHAIN" ]; then
+  echo "ERROR: unable to find Xcode cross-compiler tools."
+  [ "$0" = "${BASH_SOURCE[0]}" ] && exit 1 || return 1
+fi
+
+# XCODE_SDK is the SDK name/version being used - adjust the list as appropriate.
+# For example, remove 4.3, 6.2, and 6.1 if they are not installed. We go back to
+# the 1.0 SDKs because Apple WatchOS uses low numbers, like 2.0 and 2.1.
+XCODE_SDK=""
+for i in $(seq -f "%.1f" 30.0 -0.1 1.0)
+do
+    if [ -d "$XCODE_DEVELOPER_SDK/Developer/SDKs/$MACOS_SDK$i.sdk" ]; then
+        XCODE_SDK="$MACOS_SDK$i.sdk"
+        break
+    fi
+done
+
+# Error checking
+if [ -z "$XCODE_SDK" ]; then
+    echo "ERROR: unable to find a SDK."
+    [ "$0" = "${BASH_SOURCE[0]}" ] && exit 1 || return 1
+fi
+
+MACOS_CXXFLAGS="-arch $MACOS_CPU $MIN_VER"
+MACOS_SYSROOT="$XCODE_DEVELOPER_SDK/Developer/SDKs/$XCODE_SDK"
+
+echo "Configuring for $MACOS_SDK ($MACOS_CPU)"
+
+#####################################################################
+
+CPP=cpp; CC=clang; CXX=clang++; LD=ld
+AS=as; AR=libtool; RANLIB=ranlib;
+STRIP=strip; OBJDUMP=objdump
+
+# Error checking
+if [ ! -e "$XCODE_TOOLCHAIN/$CC" ]; then
+    echo "ERROR: Failed to find MacOS clang. Please edit this script."
+    [ "$0" = "${BASH_SOURCE[0]}" ] && exit 1 || return 1
+fi
+
+# Error checking
+if [ ! -e "$XCODE_TOOLCHAIN/$CXX" ]; then
+    echo "ERROR: Failed to find MacOS clang++. Please edit this script."
+    [ "$0" = "${BASH_SOURCE[0]}" ] && exit 1 || return 1
+fi
+
+# Error checking
+if [ ! -e "$XCODE_TOOLCHAIN/$RANLIB" ]; then
+    echo "ERROR: Failed to find MacOS ranlib. Please edit this script."
+    [ "$0" = "${BASH_SOURCE[0]}" ] && exit 1 || return 1
+fi
+
+# Error checking
+if [ ! -e "$XCODE_TOOLCHAIN/$AR" ]; then
+    echo "ERROR: Failed to find MacOS ar. Please edit this script."
+    [ "$0" = "${BASH_SOURCE[0]}" ] && exit 1 || return 1
+fi
+
+# Error checking
+if [ ! -e "$XCODE_TOOLCHAIN/$AS" ]; then
+    echo "ERROR: Failed to find MacOS as. Please edit this script."
+    [ "$0" = "${BASH_SOURCE[0]}" ] && exit 1 || return 1
+fi
+
+# Error checking
+if [ ! -e "$XCODE_TOOLCHAIN/$LD" ]; then
+    echo "ERROR: Failed to find MacOS ld. Please edit this script."
+    [ "$0" = "${BASH_SOURCE[0]}" ] && exit 1 || return 1
+fi
+
+#####################################################################
+
+# Add tools to head of path, if not present already
+LENGTH=${#XCODE_TOOLCHAIN}
+SUBSTR=${PATH:0:$LENGTH}
+if [ "$SUBSTR" != "$XCODE_TOOLCHAIN" ]; then
+    export PATH="$XCODE_TOOLCHAIN:$PATH"
+fi
+
+#####################################################################
+
+# GNUmakefile-cross and Autotools expect these to be set.
+# They are also used in the tests below. Note: at Crypto++ 8.6
+# these scripts exported CPPFLAGS, CXXFLAGS and LDFLAGS.
+export IS_MACOS=1
+
+export CPP CC CXX LD AS AR RANLIB STRIP OBJDUMP
+
+export CPPFLAGS="${DEF_CPPFLAGS} ${MACOS_CPPFLAGS} -isysroot \"${MACOS_SYSROOT}\""
+export CXXFLAGS="${DEF_CXXFLAGS} ${MACOS_CXXFLAGS} -stdlib=${MACOS_STDLIB} --sysroot \"${MACOS_SYSROOT}\""
+export LDFLAGS="${DEF_LDFLAGS} ${MACOS_LDFLAGS}"
+
+#####################################################################
+
+VERBOSE=${VERBOSE:-1}
+if [ "$VERBOSE" -gt 0 ]; then
+  echo "XCODE_TOOLCHAIN: $XCODE_TOOLCHAIN"
+  echo "MACOS_SDK: $MACOS_SDK"
+  echo "MACOS_CPU: $MACOS_CPU"
+  echo "MACOS_SYSROOT: $MACOS_SYSROOT"
+  if [ -n "${MACOS_CPPFLAGS}" ]; then
+    echo "MACOS_CPPFLAGS: ${MACOS_CPPFLAGS}"
+  fi
+  echo "MACOS_CXXFLAGS: ${MACOS_CXXFLAGS}"
+  if [ -n "${MACOS_LDFLAGS}" ]; then
+    echo "MACOS_LDFLAGS: ${MACOS_LDFLAGS}"
+  fi
+fi
+
+#####################################################################
+
+echo
+echo "*******************************************************************************"
+echo "It looks the the environment is set correctly. Your next step is build"
+echo "the library with 'make -f GNUmakefile-cross'."
+echo "*******************************************************************************"
+echo
+
+[ "$0" = "${BASH_SOURCE[0]}" ] && exit 0 || return 0
