02f4361d4780830e3e5a56430e77e9514e3817dc
Jeffrey Walton <noloader@gmail.com>
2022-08-07 10:48:37 -0400

    Update comments

--- validat9.cpp
+++ validat9.cpp
@@ -652,11 +652,11 @@ bool ValidateECGDSA(bool thorough)
 {
 	std::cout << "\nECGDSA validation suite running...\n\n";
 
-	// 'thorough' forced to false due to GH #1134. There is something
-	// sideways with GCC 12 and ECGDSA+RIPEMD. The problem is present
-	// with CRYPTOPP_DISABLE_ASM. However, Asan, UBsan and Valgrind
-	// fail to produce a finding. The program simply crashes with a
-	// junk backtrace. And GCC 11 (and earlier), Clang, MSVC are Ok.
+	// 'thorough' forced to false due to GH #1134. There is something sideways
+	// with GCC 12 and ECGDSA+RIPEMD. The problem is present with
+	// CRYPTOPP_DISABLE_ASM, which indicates a C++ problem. However, Asan,
+	// UBsan and Valgrind fail to produce a finding. The program simply crashes
+	// with a junk backtrace. And GCC 11 (and earlier), Clang, MSVC, xlC are Ok.
 	// This is likely a compiler bug.
 #if CRYPTOPP_GCC_VERSION >= 120000
 	thorough = false;
