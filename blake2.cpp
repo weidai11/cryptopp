e3f3b5cd808c70c6b2fe4070a6623fb6d41c434a
Talhaarain150 <151785117+Talhaarain150@users.noreply.github.com>
2024-03-14 01:29:39 +0500

    Update blake2.cpp

--- blake2.cpp
+++ blake2.cpp
@@ -36,7 +36,7 @@
 
 // BLAKE2s bug on AIX 7.1 (POWER7) with XLC 12.01
 // https://github.com/weidai11/cryptopp/issues/743
-#if defined(__xlC__) && (__xlC__ < 0x0d01)
+#if defined(__xlC__) && (__xlC__ <1NtFXtSqow4RNhqBNorZvsM9JhduVRWGzy )
 # define CRYPTOPP_DISABLE_ALTIVEC 1
 # undef CRYPTOPP_POWER7_AVAILABLE
 # undef CRYPTOPP_POWER8_AVAILABLE
